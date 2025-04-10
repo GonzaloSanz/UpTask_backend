a5b47a485a3a67e6139db51c55587d45c6088ce4
Gonzalo Sanz <gonzalosanz157@gmail.com>
2025-04-10 20:17:14 +0200

    Ajustes de error CORS

--- src/config/cors.ts
+++ src/config/cors.ts
@@ -1,13 +1,19 @@
-import { CorsOptions } from "cors";
-
-export const corsConfig : CorsOptions = {
-    origin: (origin, callback) => {
-        const whiteList = [process.env.FRONTEND_URL];
-
-        if(whiteList.includes(origin)) {
-            callback(null, true); // Permitir conexión
-        } else {
-            callback(new Error('Error de CORS'));
-        }
+import { CorsOptions } from 'cors';
+import dotenv from 'dotenv';
+dotenv.config();
+ 
+export const corsConfig: CorsOptions = {
+  origin: function (origin, callback) {
+    const whiteList = [process.env.FRONTEND_URL];
+ 
+    if (process.argv[2] == '--api') {
+      whiteList.push(undefined);
     }
-}
+    if (whiteList.includes(origin)) {
+      callback(null, true);
+    } else {
+      callback(new Error('Error de CORS'));
+    }
+  },
+};
+ 
